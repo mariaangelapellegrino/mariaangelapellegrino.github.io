27a6a9a4695e84139ab9f25f2a9643aaae851546
mariaangelapellegrino <mariaangelapellegrino94@gmail.com>
2026-05-23 20:34:28 +0000

    Deploying to gh-pages from @ mariaangelapellegrino/mariaangelapellegrino.github.io@b2a79f1686980ddf2e2db2328d81eb65383f8446 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Her full list of publications are available online on [Google Scholar](https://scholar.google.com/citations?user=OywXH9wAAAAJ&amp;hl=en), [Scopus](https://www.scopus.com/authid/detail.uri?authorId=57204732130), [DBLP](https://dblp.org/pid/224/0266.html).",
+          description: "Her full list of publications as indexed by DBLP.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
