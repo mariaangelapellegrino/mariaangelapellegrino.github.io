8aa86bab46e53664492db03d2312b1a3fc56cab1
mariaangelapellegrino <mariaangelapellegrino94@gmail.com>
2026-05-23 16:49:33 +0000

    Deploying to gh-pages from @ mariaangelapellegrino/mariaangelapellegrino.github.io@0e9e3552e0219cba6a8ece28e86e5fe683fc0bc4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mariaangelapellegrino/marpel.github.io",
+    "data-repo": "mariaangelapellegrino/mariaangelapellegrino.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
